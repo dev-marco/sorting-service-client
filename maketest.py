2d6380b3421c6f1684ae280543869a84a40f1ece
Marco <marcoaremail@gmail.com>
2016-06-01 18:08:39 -0300

    Fixed interpreter and added number of files as argument

--- maketest.py
+++ maketest.py
@@ -1,13 +1,13 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 
-import string, random
+import string, random, sys, json
 
 """
  * Generate a random string of lowercase letters, whitespaces and line breaks
  * @param  {integer} size String size
  * @return {string}       Resulting random string
 """
-def randomString (size):
+def randomStr (size):
 
     # Options to choose from
     random_options = string.ascii_lowercase + ' \n'
@@ -21,31 +21,49 @@ def randomString (size):
 # test if is the current running script
 if __name__ == "__main__":
 
-    # select total of fields
-    num_fields = random.randint(1, 100)
-    # select total of entries
-    num_entries = random.randint(1, 200)
-    # select total of rules
-    num_rules = for _ in range(1, 10)
+    if len(sys.argv) > 1:
 
-    # generate fields list
-    fields = [ randomString(random.randint(1, 10)) for _ in range(num_fields) ]
+        num_tests = int(sys.argv[1]) + 1
 
-    # generate entries list
-    entries = [
-        {
-            # generate a value for every field
-            field : randomString(random.randint(1, 10)) for field in fields
-        } for _ in range(num_entries)
-    ]
+        for test in range(1, num_tests):
 
-    # generate rules list
-    rules = [
-        {
-            # select a random field
-            # repetition is supported but it doesn't change results
-            'field': random.choice(fields),
-            # select a random ordem
-            'order': random.choice([ 'ASC', 'DESC' ])
-        } for _ in range(num_rules)
-    ]
+            # select total of fields
+            num_fields = random.randint(1, 100)
+            # select total of entries
+            num_entries = random.randint(1, 200)
+            # select total of rules
+            num_rules = random.randint(1, 10)
+
+            # generate fields list
+            fields = [
+                randomStr(random.randint(1, 10)) for _ in range(num_fields)
+            ]
+
+            # generate entries list
+            entries = [
+                {
+                    # generate a value for every field
+                    field : randomStr(random.randint(1, 10)) for field in fields
+                } for _ in range(num_entries)
+            ]
+
+            # generate rules list
+            rules = [
+                {
+                    # select a random field
+                    # repetition is supported but it doesn't change results
+                    'field': random.choice(fields),
+                    # select a random ordem
+                    'order': random.choice([ 'ASC', 'DESC' ])
+                } for _ in range(num_rules)
+            ]
+
+            # write entries
+            file_entries = open('test_' + str(test) + '_entries.json', 'w')
+            json.dump(entries, file_entries, indent = 4)
+            file_entries.close()
+
+            # write rules
+            file_rules = open('test_' + str(test) + '_rules.json', 'w')
+            json.dump(rules, file_rules, indent = 4)
+            file_rules.close()
