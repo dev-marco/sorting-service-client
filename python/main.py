8c72d0f18f3e4f4466afbf207bc653e83467ebaf
Marco <marcoaremail@gmail.com>
2016-06-01 17:06:21 -0300

    Python implementation

---
+++ python/main.py
@@ -0,0 +1,52 @@
+#/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sorter, json, sys
+
+"""
+ * Read json by filename
+ * @param  string       Name of the file containing JSON
+ * @return {dict|None}  Dictionary generated from JSON or None on error
+"""
+def jsonFromFileName (filename):
+    json_file = open(filename, 'r')
+
+    try:
+        rules = json.load(json_file)
+    except:
+        rules = None
+    finally:
+        # Ensures that the file is closed
+        json_file.close()
+
+    return rules
+
+# test if is the current running script
+if __name__ == "__main__":
+
+    # if has enough arguments
+    if len(sys.argv) > 2:
+
+        sort = sorter.Sorter()
+        rules = jsonFromFileName(sys.argv[1])
+
+        if rules is None:
+            # NULL rules exception
+            raise sorter.SortingServiceException('Rules set is null.')
+
+        result = []
+
+        if len(rules) > 0:
+
+            # Parse rules to Sorter
+            for rule in rules:
+                reverse = True if rule['order'] == 'DESC' else False
+                sort.addRule(rule['field'], reverse)
+
+            entries = jsonFromFileName(sys.argv[2])
+
+            # Sort result
+            result = sort.sortedIndexes(entries)
+
+        # Print result as a json encoded string
+        print(json.dumps(result))
---
+++ python/sorter.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import enum, copy, unicodedata
+
+"""
+ * Convert {string} from UTF-8 with accents to plain ASCII
+ * @param  {string} value String to be normalized
+ * @return {string}       Normalized string
+"""
+def normalize (value):
+    # Normalizes the UTF-8 string
+    normalized = unicodedata.normalize('NFD', value)
+    # Converts the string to ASCII ignoring accents
+    ascii_value = normalized.encode('ASCII', 'ignore')
+    return ascii_value
+
+class SortingServiceException (Exception):
+
+    """ Manages SortingServiceException """
+
+class Sorter:
+
+    """ Sorts a list of dictionaries by rules """
+
+    # List of tuples containing ( rule_field, rule_order )
+    __rules = []
+
+    """
+     * Insert a new rule to sort
+     * @param {string}  rule_field Name of the field to use
+     * @param {boolean} rule_reverse If this ordering is reverse
+    """
+    def addRule (self, rule_field, rule_reverse):
+        self.__rules.append( ( rule_field, rule_reverse ) );
+
+    def sortedIndexes (self, data):
+
+        # List of indexes
+        indexes = []
+        # List of the entries normalized
+        normalized_entries = []
+
+        # Fill the lists
+        for index, entry in enumerate(data):
+
+            # Create a new dictionary of normalized values
+            normalized_entry = { key: normalize(str(value)) for key, value in entry.items() }
+
+            # Append normalized entry and index to lists
+            normalized_entries.append(normalized_entry)
+            indexes.append( index )
+
+        # Python 'sorted' function is stable, so the previous ordering is not affected by this ordering when the values are equal
+        for rule_field, rule_reverse in reversed(self.__rules):
+            # Sort by the rule
+            indexes = sorted(indexes, key = lambda x: normalized_entries[ x ][ rule_field ], reverse = rule_reverse)
+
+        return indexes
